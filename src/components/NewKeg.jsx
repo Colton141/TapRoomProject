2f40bb7599bce173ac97b2eeb64b19f5fdd380a5
colton lacey <coltnutmiles14@gmail.com>
2019-07-09 12:46:57 -0700

    update new keg form

---
+++ src/components/NewKeg.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Login(){
+  return (
+    <div>
+      <h1>New Keg</h1>
+      <form>
+        <input type='text' id='name' placeholder='NAME'/>
+        <input type='text' id='brand' placeholder='BRAND'/>
+        <input type='text' id='price' placeholder='PRICE'/>
+        <input type='text' id='alcoholContent' placeholder='ALCOHOLCONTENT'/>
+        <Link to="/"><button type='submit'>Add Keg</button></Link>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
