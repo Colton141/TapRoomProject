89f268b103630a6e5827d6c9125685700862df82
colton lacey <coltnutmiles14@gmail.com>
2019-07-12 11:54:57 -0700

    fix keg control

--- src/components/NewKegControl.jsx
+++ src/components/NewKegControl.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
-import NewTicketForm from './NewTicketForm';
+import NewKegForm from './NewKegForm';
 import PropTypes from 'prop-types';
 
-class NewTicketControl extends React.Component {
+class NewKegControl extends React.Component {
 
   constructor(props) {
     super(props);
@@ -20,7 +20,7 @@ class NewTicketControl extends React.Component {
   render() {
     let currentlyVisibleContent = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation} />;
+      currentlyVisibleContent = <NewKegForm onNewKegCreation={this.props.onNewKegCreation} />;
     } else {
       currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation} />;
     }
@@ -32,6 +32,6 @@ class NewTicketControl extends React.Component {
   }
 }
 
-NewTicketControl.propTypes = {
-  onNewTicketCreation: PropTypes.func
+NewKegControl.propTypes = {
+  onNewKegCreation: PropTypes.func
 };
