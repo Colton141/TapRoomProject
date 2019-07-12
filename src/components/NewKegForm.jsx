bd66a5955a9629682ab0efd3f29b673cf8363bd4
colton lacey <coltnutmiles14@gmail.com>
2019-07-12 14:59:58 -0700

    import proptype into newkegform

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
 function NewKegForm(props){
   let _names = null;
