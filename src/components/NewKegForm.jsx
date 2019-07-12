5b4b9c6114007a2454568585986cad1175ca8666
colton lacey <coltnutmiles14@gmail.com>
2019-07-12 14:40:01 -0700

    change newkegform to take user imput

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function NewKeg(){
+function NewKegForm(props){
+  let _names = null;
+  let _brand = null;
+  let _price = null;
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({names: _names.value, brand: _brand.value, price: _price.value, alcoholContent: _alcohholContent.value, id: v4()});
+    _names.value = '';
+    _brand.value = '';
+    _price.value = '';
+    _alcohholContent.value = '';
+  }
+
   return (
     <div>
-      <h1>New Keg</h1>
-      <form>
-        <input type='text' id='name' placeholder='NAME'/>
-        <input type='text' id='brand' placeholder='BRAND'/>
-        <input type='text' id='price' placeholder='PRICE'/>
-        <input type='text' id='alcoholContent' placeholder='ALCOHOLCONTENT'/>
-        <Link to="/"><button type='submit'>Add Keg</button></Link>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          id='names'
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
+        <input
+          type='text'
+          id='brand'
+          placeholder='Location'
+          ref={(input) => {_brand = input;}} />
+        <textarea
+          id='price'
+          placeholder='Describe your price.'
+          ref={(textarea) => {_price = textarea;}} />
+          <input
+            type='text'
+            id='brand'
+            placeholder='Location'
+            ref={(input) => {_alcohholContent = input;}} />
+        <button type='submit'>Help!</button>
       </form>
     </div>
   );
