d2263e18274359b591570a8871371eaaafe7dbc3
colton lacey <coltnutmiles14@gmail.com>
2019-07-12 10:00:28 -0700

    remove keg list from list

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -2,50 +2,7 @@ import React from 'react';
 import Keg from './Keg';
 
 const masterKegList = [
-  {
-
-    name: 'PBR',
-    brand: 'PBR',
-    price: '6',
-    alcoholContent: '4.5'
-  },
-  {
-    name: 'Coors Lite',
-    brand: 'Coors',
-    price: '6',
-    alcoholContent: '4.2'
-  },
-  {
-    name: 'Miller Lite',
-    brand: 'Miller',
-    price: '6',
-    alcoholContent: '4.2'
-  },
-  {
-    name: 'Blue Moon',
-    brand: 'Blue Moon',
-    price: '8',
-    alcoholContent: '5.4'
-  },
-  {
-    name: 'Amber Ale',
-    brand: 'Fat Tire',
-    price: '9',
-    alcoholContent: '5.6'
-  },
-  {
-    name: 'Rainier',
-    brand: 'Rainier',
-    price: '8',
-    alcoholContent: '4.6'
-  },
-  {
-    name: 'Bud Ligh',
-    brand: 'Budwieser',
-    price: '5',
-    alcoholContent: '6'
-  }
-];
+  
 
 function KegList() {
   return (
@@ -57,7 +14,7 @@ function KegList() {
           price={keg.price}
           alcoholContent={keg.alcoholContent}
           key={index} />
-          
+
       )}
     </div>
   );
